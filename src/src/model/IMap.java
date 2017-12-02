f668ffc5a961544cabd86ea1cd6709f4156a353c
pag5 <pag5@st-andrews.ac.uk>
2017-12-02 11:12:56 +0000

    Updated map interface

--- src/src/model/IMap.java
+++ src/src/model/IMap.java
@@ -1,7 +1,29 @@
 package model;
 
+import shared.Block;
+
 public interface IMap {
 
-    char[][] getMap();
+    /**
+     * Gets the map 2D array
+     * @return the map 2D array
+     */
+    Block[][] getMap();
+
+    /**
+     * Updates the tile at (x, y) to block
+     * @param x x coord
+     * @param y y coord
+     * @param block the block
+     */
+    void updateTile(int x, int y, Block block);
+
+    /**
+     * Gets the block at (x, y)
+     * @param x x coord
+     * @param y y coord
+     * @return the block at (x, y)
+     */
+    Block getTileAt(int x, int y);
 
 }
