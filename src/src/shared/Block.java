a6306cadb886a88d3b97990e2d0dd79b8eaa66d0
pag5 <pag5@st-andrews.ac.uk>
2017-12-02 11:17:54 +0000

    Added block definiations

--- src/src/shared/Block.java
+++ src/src/shared/Block.java
@@ -1,4 +1,23 @@
 package shared;
 
 public class Block {
+
+    /**
+     * FLOOR 'f'
+     * LAVA 'l'
+     * WALL 'w'
+     * SPIKE 's'
+     * DOOR 'd'
+     * FINISH 'Y'
+     * key 'k'
+     */
+
+    public static char floor = 'f';
+    public static char lava = 'l';
+    public static char wall = 'w';
+    public static char spike = 's';
+    public static char door = 'd';
+    public static char finish = 'Y';
+    public static char key = 'k';
+
 }
--- src/src/view/Render.java
+++ src/src/view/Render.java
@@ -4,14 +4,18 @@ import java.awt.*;
 
 public class Render {
 
+    private Dimension size;
     private Graphics g;
 
-    public Render(Graphics g) {
+    public Render(Graphics g, Dimension size) {
         this.g = g;
+        this.size = size;
     }
 
     public void renderText(String text) {
-        g.drawRect();
+        g.drawRect(size.height);
     }
+
+
 
 }
