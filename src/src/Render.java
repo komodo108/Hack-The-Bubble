ccd7daacba25ab910c79f600e1d2bd98097056dc
pag5 <pag5@st-andrews.ac.uk>
2017-12-02 10:46:12 +0000

    Made initial view classes

---
+++ src/src/Render.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class Render {
+
+    private Graphics g;
+
+    public Render(Graphics g) {
+        this.g = g;
+    }
+
+    public void renderText(String text) {
+        g.drawRect();
+    }
+
+}
